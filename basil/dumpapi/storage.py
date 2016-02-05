2db5d8779d2c66c76dc7f47348a837229bc863da
Steve Lewis <stevelle@gmail.com>
2016-02-04 19:02:37 -0800

    Fixing session timeout issue with db

--- basil/dumpapi/storage.py
+++ basil/dumpapi/storage.py
@@ -6,6 +6,7 @@ from sqlalchemy.ext.declarative import declarative_base
 
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm.scoping import scoped_session
 
 logging = logging.getLogger(__name__)
 Base = declarative_base()
@@ -56,14 +57,31 @@ class DBSessionFactory(object):
         self.sessions = sessions
 
     def process_request(self, req, resp):
+        logging.debug('Setting up session')
         req.context['session'] = self.sessions()
 
     def process_response(self, req, resp, resource):
-        req.context['session'].close()
+        try:
+            # TODO look up a better way to do this /if/
+            resp_status = int(resp.status.split(' ', 1)[0])
+            if resp_status in [201, 202, 204]:
+                try:
+                    logging.debug('Committing')
+                    req.context['session'].commit()
+                except Exception as ex:
+                    logging.warn(ex.message)
+                    logging.debug('Rolling Back due to sql error')
+                    raise ex
+            elif resp_status >= 400:
+                logging.debug('Rolling Back: error status [%d]', resp_status)
+            else:
+                logging.debug('Rolling Back: read-only operation')
+        finally:
+            self.sessions.remove()
 
 
 def prepare_storage(connect_str):
-    engine = create_engine(connect_str)
-    return sessionmaker(bind=engine)
+    engine = create_engine(connect_str, pool_recycle=7200)
+    return scoped_session(sessionmaker(bind=engine))
 
 
