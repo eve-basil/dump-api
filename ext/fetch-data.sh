c9c550482bab30c97a0f740208ed9abff74d3285
Steve Lewis <stevelle@gmail.com>
2016-03-08 01:08:14 -0800

    Add caching for travis.ci

--- ext/fetch-data.sh
+++ ext/fetch-data.sh
@@ -2,12 +2,17 @@
 set -ev
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-mkdir ${DIR}/../data
+if [ ! -d "${DIR}/../data" ];then
+    mkdir ${DIR}/../data
+fi
 
 pushd ${DIR}/../data
-  wget https://www.fuzzysteve.co.uk/dump/latest/eve.db.bz2 --no-check-certificate -q
-  bunzip2 eve.db.bz2
-
-  wget https://www.fuzzysteve.co.uk/dump/latest/blueprints.yaml.bz2 --no-check-certificate -q
-  bunzip2 blueprints.yaml.bz2
+  if [ ! -f "eve.db" ]; then
+    wget https://www.fuzzysteve.co.uk/dump/latest/eve.db.bz2 --no-check-certificate -q
+    bunzip2 eve.db.bz2
+  fi
+  if [ ! -f "blueprints.yaml" ]; then
+    wget https://www.fuzzysteve.co.uk/dump/latest/blueprints.yaml.bz2 --no-check-certificate -q
+    bunzip2 blueprints.yaml.bz2
+  fi
 popd
