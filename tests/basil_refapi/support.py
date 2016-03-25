0c0de5ab423d75804a5e9fb961d1dc8e866c1b18
Steve Lewis <stevelle@gmail.com>
2016-03-25 10:29:27 -0700

    add tests for activities api endpoints
    Uses hamcrest differently to make syntax cleaner

--- tests/basil_refapi/support.py
+++ tests/basil_refapi/support.py
@@ -3,6 +3,9 @@ import os
 import basil_common.db as db
 import tests.basil_refapi
 
+from basil_common.falcon_support import InjectorMiddleware
+from basil_refapi import recipes
+
 
 def test_db_conn():
     data_path = os.path.normpath(tests.basil_refapi.__path__[0] + '/../..')
@@ -12,3 +15,13 @@ def test_db_conn():
 def session_maker():
     conn_str = test_db_conn()
     return db.prepare_storage(connect_str=conn_str, conn_timeout=600)
+
+
+def test_cache_conn():
+    return recipes.bootstrap_store(os.environ['REDIS_HOST'],
+                                   os.environ['REDIS_PASSWORD'])
+
+
+def cache_middleware():
+    recipe_proxy = recipes.Recipes(test_cache_conn())
+    return InjectorMiddleware({'recipes': recipe_proxy})
---
+++ tests/basil_refapi/test_api_activities.py
@@ -0,0 +1,94 @@
+import falcon
+import pytest
+
+from basil_refapi import api
+from tests import *
+from tests.basil_refapi import support
+
+
+@pytest.fixture(scope="module")
+def app():
+    return api.create_api([support.cache_middleware()])
+
+application = app()
+
+
+def test_get_manufacturing_by_id(client):
+    resp = client.get('/recipes/manufacturing/691')
+    assert_that(resp, has_property('status'), falcon.HTTP_OK)
+
+    assert_that(resp.json, has_entry('time', 6000))
+
+    assert_that(resp.json, has_key('materials'))
+    assert_that(resp.json['materials'], has_length(7))
+
+    assert_that(resp.json, has_key('products'))
+    assert_that(resp.json['products'], has_length(1))
+    assert_that(resp.json['products'][0], has_entry('typeID', 587))
+    assert_that(resp.json['products'][0], has_entry('quantity', 1))
+
+
+def test_get_manufacturing_by_bad_id(client):
+    resp = client.get('/recipes/manufacturing/1')
+    assert_that(resp, has_property('status'), falcon.HTTP_NOT_FOUND)
+
+
+def test_get_invention_by_id(client):
+    resp = client.get('/recipes/invention/691')
+    assert_that(resp, has_property('status'), falcon.HTTP_OK)
+
+    assert_that(resp.json, has_entry('time', 63900))
+
+    assert_that(resp.json, has_entry('materials', instance_of(list)))
+    assert_that(resp.json['materials'], has_length(2))
+
+    assert_that(resp.json, has_key('products'))
+    prod = resp.json['products']
+    assert_that(prod, has_length(2))
+    assert_that(prod, has_item(has_entry('typeID', 11372)))
+    assert_that(prod, has_item(has_entry('quantity', 1)))
+
+
+def test_get_invention_by_bad_id(client):
+    resp = client.get('/recipes/invention/1')
+    assert_that(resp, has_property('status'), falcon.HTTP_NOT_FOUND)
+
+
+def test_get_copying_by_id(client):
+    resp = client.get('/recipes/copying/691')
+    assert_that(resp, has_property('status'), falcon.HTTP_OK)
+    assert_that(resp.json, has_entry('time', 4800))
+
+
+def test_get_copying_by_bad_id(client):
+    resp = client.get('/recipes/copying/1')
+    assert_that(resp, has_property('status'), falcon.HTTP_NOT_FOUND)
+
+
+def test_research_material_by_id(client):
+    resp = client.get('/recipes/research_material/691')
+    assert_that(resp, has_property('status'), falcon.HTTP_OK)
+    assert_that(resp.json, has_entry('time', 2100))
+
+
+def test_get_research_material_by_bad_id(client):
+    resp = client.get('/recipes/research_material/1')
+    assert_that(resp, has_property('status'), falcon.HTTP_NOT_FOUND)
+
+
+def test_research_time_by_id(client):
+    resp = client.get('/recipes/research_time/691')
+    assert_that(resp, has_property('status'), falcon.HTTP_OK)
+    assert_that(resp.json, has_entry('time', 2100))
+
+
+def test_get_research_time_by_bad_id(client):
+    resp = client.get('/recipes/research_time/1')
+    assert_that(resp, has_property('status'), falcon.HTTP_NOT_FOUND)
+
+
+def test_get_unknown_activity_by_id(client):
+    resp = client.get('/recipes/unknown/1')
+    assert_that(resp, has_property('status'), falcon.HTTP_NOT_FOUND)
+
+# FUTURE more tests
